a3f501609cae7d28ba4d5f134583b9fdb0c45763
klozz <xxx.reptar.rawrr.xxx@gmail.com>
2014-08-14 00:51:23 -0500

    removed unused files
    Change-Id: I7faad7c9384889b0daeca67e7bd907575f73a0a4
    Signed-off-by: klozz <xxx.reptar.rawrr.xxx@gmail.com>

--- xpe.mk
+++
@@ -1,9 +0,0 @@
-$(call inherit-product, device/motorola/falcon/full_falcon.mk)
-
-# Inherit some common XPe stuff.
-$(call inherit-product, vendor/XPerience/config/common_full_phone.mk)
-
-PRODUCT_RELEASE_NAME := MOTO G
-PRODUCT_NAME := xpe_falcon
-
-
